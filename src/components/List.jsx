e2ce925d4940c01bacea18788432b3cac338845f
Shugmi Shumunov <SShumunov@gmail.com>
2024-01-05 10:32:07 -0500

    feat: Allow for selection of ListItems
    - solves for #1 & #2

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,22 +1,42 @@
+import { useState } from 'react';
+
 import './List.css';
 
 // Implement a feature to allow item selection with the following requirements:
-// 1. Clicking an item selects/unselects it.
-// 2. Multiple items can be selected at a time.
+// ~~1. Clicking an item selects/unselects it.~~
+// ~~2. Multiple items can be selected at a time.~~
 // 3. Make sure to avoid unnecessary re-renders of each list item in the big list (performance).
 // 4. Currently selected items should be visually highlighted.
 // 5. Currently selected items' names should be shown at the top of the page.
 //
 // Feel free to change the component structure at will.
 
-export const List = ({ items }) => (
-  <>
-    <ul className="List">
-      {items.map(item => (
-        <li key={item.name} className={`List__item List__item--${item.color}`}>
-          {item.name}
-        </li>
-      ))}
-    </ul>
-  </>
-);
+export const List = ({ items }) => {
+  const [selectedItems, setSelectedItems] = useState([]);
+
+  const handleListItemClick = (item) => {
+    if (selectedItems.includes(item)) {
+      setSelectedItems(selectedItems.filter((selectedItem) => selectedItem !== item));
+    } else {
+      setSelectedItems([...selectedItems, item]);
+    }
+  }
+
+  console.log("these are selected", selectedItems);
+
+  return (
+    <>
+      <ul className="List">
+        {items.map(item => (
+          <li
+            key={item.name}
+            className={`List__item List__item--${item.color}`}
+            onClick={() => handleListItemClick(item)}
+          >
+            {item.name}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
