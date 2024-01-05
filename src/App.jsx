ee4b151bcdb7259fa47cdc7992e26a8c66fe288d
Shugmi Shumunov <SShumunov@gmail.com>
2024-01-05 10:11:58 -0500

    chore: Setup project w/ React & Vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>React App</h1>
+      <p>'Ello World</p>
+    </>
+  )
+}
+
+export default App
